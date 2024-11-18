019e2142b682e7abe479dbcf6fdecde12176c06b
Simran <simrandhanda577@gmail.com>
2024-11-18 22:20:51 +0530

    init commit

---
+++ src/main/java/com/weatherforecast/ForecastService.java
@@ -0,0 +1,5 @@
+package com.weatherforecast;
+
+public class ForecastService {
+
+}
---
+++ src/main/java/com/weatherforecast/WeatherApp.java
@@ -0,0 +1,7 @@
+package com.weatherforecast;
+
+public class WeatherApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Weather Forecast System!");
+    }
+}
